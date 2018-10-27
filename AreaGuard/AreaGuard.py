3d52efdd2072e14a1ac52bbad9f0bd9ac67bf824
proshare17 <proshare.17@gmail.com>
2018-10-28 00:06:53 +0900

    一時保存

--- AreaGuard/AreaGuard.py
+++ AreaGuard/AreaGuard.py
@@ -2,6 +2,7 @@ import pygame, math, random, sys, re
 from pygame.locals import *  # お呪い　不要？
 
 GAME_MODE = {'START': 0, 'PLAY': 1, 'GAMEOVER': 2}
+# BARRIER_MODE = {'RED': 0, 'GREEN': 1, 'YELLOW': 2}
 SCR_RECT = Rect(0, 0, 576, 768)  # スクリーンサイズ
 
 
@@ -24,13 +25,17 @@ class Game:
 
     def init_game(self):
         self.game_state = GAME_MODE['START']
+        # self.barrier_color = BARRIER_MODE['GREEN']
         self.all_sprite = pygame.sprite.RenderUpdates()
+        # self.barrier = pygame.sprite.Group()
         self.pc = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
         self.razers = pygame.sprite.Group()
+        # Barrier.containers = self.all_sprite, self.barrier
         Player.containers = self.all_sprite, self.pc
         Enemy.containers = self.all_sprite, self.enemies
         Razer.containers = self.all_sprite, self.razers
+        #self.barrier = Barrier()
         self.player = Player()
         self.score = 0
 
@@ -68,15 +73,18 @@ class Game:
             # GAME OVERを描画
             gameover_font = pygame.font.SysFont(None, 80)
             gameover = gameover_font.render("GAME OVER", False, (255, 0, 0))
-            screen.blit(gameover, ((SCR_RECT.width - gameover.get_width()) / 2, 200))
+            screen.blit(gameover, ((SCR_RECT.width - gameover.get_width()) / 2,
+                                   (SCR_RECT.height - gameover.get_height()) / 4))
             # PUSH STARTを描画
             push_font = pygame.font.SysFont(None, 40)
             push_space = push_font.render("PUSH SPACE KEY", False, (255, 255, 255))
-            screen.blit(push_space, ((SCR_RECT.width - push_space.get_width()) / 2, 400))
+            screen.blit(push_space, ((SCR_RECT.width - push_space.get_width()) / 2,
+                                     (SCR_RECT.height - push_space.get_height()) * 3 / 4))
             # 得点表示
-            score_font = pygame.font.SysFont(None, 80)
-            score = score_font.render("You got {0:0>4d} points!".format(self.score), False, (255, 255, 255))
-            screen.blit(score, ((SCR_RECT.width - score.get_width()) / 2, 300))
+            score_font = pygame.font.SysFont(None, 60)
+            score = score_font.render("You killed {0:0>4d} enemies!".format(self.score), False, (255, 255, 255))
+            screen.blit(score, ((SCR_RECT.width - score.get_width()) / 2,
+                                (SCR_RECT.height - score.get_height()) / 2))
 
     def collision_detection(self):
         player_collided = pygame.sprite.groupcollide(self.enemies, self.pc, True, True)
@@ -87,11 +95,23 @@ class Game:
         for razer in razer_collided.keys():
             self.score += 1
 
+        """barrier_collided = pygame.sprite.groupcollide(self.enemies, self.barrier, True, True)
+        for barrier in barrier_collided.keys():
+            if self.barrier_color == BARRIER_MODE['GREEN']:
+                self.barrier_color = BARRIER_MODE['YELLOW']
+            if self.barrier_color == BARRIER_MODE['YELLOW']:
+                self.barrier_color = BARRIER_MODE['RED']
+            if self.barrier_color == BARRIER_MODE['RED']:
+                self.game_state = GAME_MODE['GAMEOVER']"""
+
     def load_images(self):
         # スプライトの画像を登録
         Player.image = load_image("pc_img.png")
         Enemy.image = load_image("enemy_img.png")
         Razer.image = load_image("razer_img.png")
+
+    # Barrier.image = load_image("barrier_green.png")
+
 
     def key_handler(self):
         for event in pygame.event.get():
@@ -110,31 +130,31 @@ class Game:
 
 
 class Player(pygame.sprite.Sprite):
-    speed = 3  # 移動速度
+    speed = 4  # 移動速度
     charge = 15  # レーザーがチャージされるまでの時間
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.rect = self.image.get_rect()
         self.rect.bottom = SCR_RECT.bottom  # プレイヤーは画面の一番下からスタート
-        self.rect.left = 400
+        self.rect.left = (SCR_RECT.width - self.rect.width) / 2
         self.charge_timer = 0
 
     def update(self):
         pressed_key = pygame.key.get_pressed()
-        if pressed_key[K_UP]:
-            self.rect.move_ip(0, -self.speed)
         if pressed_key[K_RIGHT]:
             self.rect.move_ip(self.speed, 0)
-        if pressed_key[K_DOWN]:
-            self.rect.move_ip(0, self.speed)
         if pressed_key[K_LEFT]:
             self.rect.move_ip(-self.speed, 0)
+        if pressed_key[K_UP]:
+            self.rect.move_ip(0, -self.speed)
+        if pressed_key[K_DOWN]:
+            self.rect.move_ip(0, self.speed)
         # 画面からはみ出さないようにする
         self.rect = self.rect.clamp(SCR_RECT)
 
         if pressed_key[K_SPACE]:
-            # リロード時間が0になるまで発射できない。
+            # 連射時リロード時間が0になるまで発射できない。
             if self.charge_timer > 0:
                 # リロード中
                 self.charge_timer -= 1
@@ -142,7 +162,24 @@ class Player(pygame.sprite.Sprite):
                 # 発射！
                 Razer(self.rect.center, self.rect.top)  # 作成すると同時にall_spriteに追加される。
                 self.charge_timer = self.charge
+        else:  # 連射終了、リロード
+            self.charge_timer = 0
 
+
+"""class Barrier(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.rect = self.image.get_rect()
+        self.rect.left = SCR_RECT.left
+        self.rect.bottom = SCR_RECT.bottom
+
+
+    def update(self):
+        if Game.barrier_color == BARRIER_MODE['YELLOW']:
+            self.image = load_image("barrier_yellow.png")
+        if Game.barrier_color == BARRIER_MODE['RED']:
+            self.image = load_image("barrier_red.png")"""
 
 class Enemy(pygame.sprite.Sprite):
     speed = 3  # 移動速度
